63176b05d91c76bc28e38d7f0f2641b48f47ebe5
Ari Lacenski <alacenski@gmail.com>
2012-04-17 23:45:28 -0700

    Fetching Face.com tags

--- checkdir.py
+++ checkdir.py
@@ -1,4 +1,4 @@
-# #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 
 # checkdir.py
 # by tensory 
@@ -10,39 +10,35 @@
 # poster, face_client, httplib2, simplejson
 # Install poster from http://pypi.python.org/pypi/poster/0.4
  
-import os, sched, time, urlparse
+import sys, os, sched, time, urlparse
 import httplib2, simplejson as json
 import face_client
 import pprint # not required, get rid of later
 
-# Get the app access token from Facebook 
-def getAppAccessToken(app_id, secret):
-    url = 'https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&grant_type=client_credentials' % (app_id, secret)
-    response, content = httplib2.Http().request(url)
-    return content
 
-def getUserAccessToken(app_id, redirect, state, http):
-    scopes = ['friends_photos', 'friends_photo_video_tags', 'offline_access']
-    url = 'https://www.facebook.com/dialog/oauth?client_id=%s&redirect_uri=%s&scope=%s&state=%s'
-    url = url % (app_id, redirect, ','.join(scopes), state)
-    print http.request(url)
-    return ''
-
-def getFriendsUrl(user):
+def getFriendsUrl(user, token):
     # TODO: This token must be generated.
     # Meantime: get it from http://developers.facebook.com/docs/reference/api/ logged in as mrnightmarket user
     # and then follow instructions at http://itslennysfault.com/get-facebook-access-token-for-graph-api to slurp it up
     # when this thing has a domain.
-    token = 'AAAAAAITEghMBAP53uwyNmbd997sKimHsZBdTLTPQJG2c9I6zDsgXuLrTJgkIm30aos9LLbU8IvHXiZCZA0LAZB65zLKxPCKiWG3XK6pZBR6ZCGHAlztFmj'
     base = 'https://graph.facebook.com/%s/friends?access_token=%s&limit=5000&offset=0'
     return base % (user, token)
+
+token = ''
+# Needs a valid FB API token
+if (len(sys.argv) > 1):
+    token = sys.argv[1]
+else: 
+    print "Need an API token! :("
+    sys.exit()
 
 # remove
 pp = pprint.PrettyPrinter(indent=4)
 
 path = '/Users/alacenski/Documents/Projects/Face/bin' # TODO: change this to run in the current directory
 knownFiles = []
-fb = {
+
+fb = { 
     'login':'mrnightmarket@stupidthing.org',
     'password':'QZ2YHcwHfi2DBJSPJNhE',
     'username':'mrnightmarket',
@@ -51,30 +47,40 @@ fb = {
 
 fbApi = {
     'app_id':'199009273549135',
-    'secret':'78fe31e68c7f856cd95f4e42a209c222'
+    'secret':'78fe31e68c7f856cd95f4e42a209c222',
+    'token': token
 }
+
 faceApi = {
     'key':'ebfcff03e728bd6d8c60b53d11cc6a7b',
     'password':'27a6b645c50832cc6c537a6852957f97'
 }
 
+# Grab pictures at this URL
+fbPublicDataBase = 'https://graph.facebook.com/%s?fields=picture&type=large'
+
 http = httplib2.Http()
 # Set up face.com client
-faceClient = face_client.FaceClient(faceApi['key'], faceApi['password'])
-
-# Get API access token
-state = 'foo'
-token = getUserAccessToken(fbApi['app_id'], fb['domain'], state, http)
+client = face_client.FaceClient(faceApi['key'], faceApi['password'])
 
 # Reload the LHNM Facebook user's friend list to keep training the face checker
-#friendsJsonUrl = getFriendsUrl(fb['username'])
-#result, content = http.request(friendsJsonUrl)
-#friends = json.loads(content)['data'] # Get just the friends 'data' object from response JSON
+friendsJsonUrl = getFriendsUrl(fb['username'], token)
+result, content = http.request(friendsJsonUrl)
+friends = json.loads(content)['data'] # Get just the friends 'data' object from response JSON
 #pp.pprint(friends) # yay
 
+for friend in friends:
+    response, data = http.request(fbPublicDataBase % friend['id'])
+    url = json.loads(data)
+    # Upload their photo to the Face namespace
+    faceResponse = client.faces_detect(url['picture'])
+    # Get Face tags
+    for photo in faceResponse['photos']:
+        #print photo
+        if (len(photo['tags']) > 0):
+            print [photo['tags'][0]['tid']]
 
-# start downloading people's usericons
-#for friend in friends:
+
 #    fData = json.loads(friend)
 #    print friend['id']
 
