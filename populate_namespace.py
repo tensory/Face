d2d17fe5e89364c0d16f1100a935ca585e27e8f5
Michael Toren <mct@bittorrent.com>
2012-04-21 15:01:04 -0700

    Progress

--- populate_namespace.py
+++ populate_namespace.py
@@ -1,67 +1,116 @@
 #!/usr/bin/env python
-# change to your local Python
 #
-# checkdir.py
-# by tensory 
+# train.py
+# by tensory, mct
 #
-# Watches a directory for new jpgs, attempts to match them 
-# to a Face.com namespace
+# Reads a list of facebook IDs from command line arguments, queries Facebook
+# for their profile picture, copied the picture to a local directory, and then
+# attempts to contact face.com to add it to our training corpus.
 #
-# Non-Python dependencies: 
-# poster, face_client, httplib2, simplejson
-# Install poster from http://pypi.python.org/pypi/poster/0.4
+# not pretty, but we're in a hurry :)
  
-import sys, re, os, sched, time, urlparse
-import httplib2, simplejson as json
+import sys
+import urllib
+import json
 import face_client
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 if len(sys.argv) < 2:
     print "Usage: %s <id> [id...]" % sys.argv[0]
     sys.exit(0)
 
-training_dir = os.path.realpath(os.path.dirname(__file__) + "/training")
-
 fbPublicDataBase = 'http://graph.facebook.com/%s?fields=id,link,name,picture&type=large' # *NOT* https
-
-print "Using trainining directory of", training_dir
 
 faceApi = {
     'key':'ebfcff03e728bd6d8c60b53d11cc6a7b',
     'password':'27a6b645c50832cc6c537a6852957f97',
-    'namespace':'surveillancetruck'
+    'namespace':'nighttest'
 }
 
-http = httplib2.Http()
+print "Populating namespace", faceApi['namespace']
 
 print "Logging in to face.com"
-client = face_client.FaceClient(faceApi['key'], faceApi['password'])
+face_client.set_log_level(9)
+client = face_client.FaceClient(faceApi['key'], faceApi['password'], ssl=False)
+
+counter_ok = 0
+counter_no_id = 0
+counter_no_face = 0
+counter_low_confidence = 0
+counter_face_fail = 0
 
 for id in sys.argv[1:]:
-    id = int(id)
     print
     print "Contacting Facebook for JSON for", id
-    response, data = http.request(fbPublicDataBase % id)
-    j = json.loads(data)
+    content = urllib.urlopen(fbPublicDataBase % id).read()
+    j = json.loads(content)
 
     if "id" not in j:
         print "Field 'id' missing?  Skipping"
+        counter_no_id += 1
         continue
+
+    print "Facebook ID is", j['id']
 
     if "picture" not in j:
         print "Field 'picture' missing?  Skipping"
+        counter_no_id += 1
         continue
 
-    print "Sending", j['picture'], "to Face.com"
-    faceResponse = client.faces_detect(j['picture'])
+    print "Fetching", j['picture']
+    imagedata = urllib.urlopen(j['picture']).read()
 
-    for photo in faceResponse['photos']:
-        if len(photo['tags']) > 0:
-            userTag = [photo['tags'][0]['tid']]
-            tag = '%s@%s' % (id, faceApi['namespace'])
-            print "Calling tags_save on", tag
-            try:
-                client.tags_save(tids=userTag, uid=tag, label=j['id'])
-                client.faces_train(tag)
-                print "Ok"
-            except Exception as e:
-                print "Failed:", str(e)
+    localfile = "profilepictures/%s.jpg" % j['id']
+    print "Writing to", localfile
+    open(localfile, "w").write(imagedata)
+
+    print "Calling API faces_detect"
+    face_response = client.faces_detect(file_name=localfile)
+
+    if not face_response['photos']:
+        print "No photos?"
+        continue
+
+    #pp.pprint(face_response)
+
+    for photo in face_response['photos']:
+        if not photo['tags']:
+            print "No tags found"
+            counter_no_face += 1
+            continue
+
+        confidence = photo['tags'][0]['attributes']['face']['confidence']
+        print "Face confidence", confidence
+        if confidence < 60:
+            print "Skipping low confidence"
+            counter_low_confidence += 1
+            continue
+
+        # TID is the temporary ID for the face that face.com detected in the photo
+        # We now want to associate it with a UID we define, which will be returned if we recognize this person later
+        tid = photo['tags'][0]['tid']
+        uid = '%s@%s' % (j['id'], faceApi['namespace'])
+
+        try:
+            print "Calling tags_save on", uid
+            client.tags_save(tids=tid, uid=uid)
+
+            print "Calling faces_train on", uid
+            client.faces_train(uid)
+
+            print "Ok"
+            counter_ok += 1
+
+        except Exception as e:
+            print "Failed:", str(e)
+            counter_face_fail += 1
+            sys.exit()
+
+
+print
+print "OK", counter_ok
+print "No ID", counter_no_id
+print "No Face", counter_no_face
+print "Low confidence", counter_low_confidence
+print "Face fail", counter_face_fail
